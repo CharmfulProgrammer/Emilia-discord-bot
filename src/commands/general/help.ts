569163ef007a8a42b9062f0d4891a4c132222912
NajeebFarhan <najeebfarhan6345@gmail.com>
2021-03-10 16:53:43 +0530

    half help

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -1,7 +1,6 @@
 import Command from "../../selfUtils/commandFrame";
 import {commandInfo} from "../../selfUtils/commandHandler";
-import {MessageEmbed} from "discord.js"
-import config from "../../config";
+import {Message, MessageEmbed} from "discord.js"
 
 export const command = new Command(
     "help",
@@ -10,36 +9,46 @@ export const command = new Command(
     async(message, args) => {
         try {
             if(args.length) {
-                let infos;
-                commandInfo.forEach(cmd => {
-                    if(cmd.find(name => name.name === args[0])){
-                        infos = cmd.find(name => name.name === args[0]);
-                    };
-                });
-                const embed: Partial<MessageEmbed> = {
-                    color: 0x034efc,
-                    title: infos.name,
-                    description: infos.description,
-                    fields: [
-                        {
-                            name: "Category",
-                            value: infos.category,
-                            inline: false
-                        },
-                        {
-                            name: "Arguments",
-                            value: infos.args,
-                            inline: false
-                        },
-                    ],
-                    footer: {
-                        text: "user: user id|mention|name"
-                    }
-                }
-                message.channel.send({embed});
+                specificHelp(message, args);
+            }else {
+                allHelp(message, args);
             }
         } catch {
             
         }
     }
 );
+
+const specificHelp = (message: Message, args: string[]) => {
+    let infos;
+    commandInfo.forEach(cmd => {
+        if(cmd.find(name => name.name === args[0])){
+            infos = cmd.find(name => name.name === args[0]);
+        };
+    });
+        const embed: Partial<MessageEmbed> = {
+            color: 0x034efc,
+            title: infos.name,
+            description: infos.description,
+            fields: [
+                {
+                    name: "Category",
+                    value: infos.category,
+                    inline: false
+                },
+                {
+                    name: "Arguments",
+                    value: infos.args,
+                    inline: false
+                },
+            ],
+            footer: {
+                text: "user: user id|mention|name"
+            }
+        }
+        message.channel.send({embed});
+}
+
+const allHelp = (message: Message, args: string[]) => {
+    console.log(commandInfo[0]);
+}
