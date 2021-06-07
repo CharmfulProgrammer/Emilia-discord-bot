b3c6f1dfb018e3d7cfbe8729966aa7e309cc2e83
NajeebFarhan <54494073+CharmfulProgrammer@users.noreply.github.com>
2021-06-07 16:28:43 +0530

    im happy

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,28 @@
 import { Client, Message } from "discord.js";
 import dotenv from "dotenv";
+import commands from "./libs/command";
 
 dotenv.config();
 const client = new Client();
-const PREFIX = process.env.PREFIX
+const PREFIX = process.env.PREFIX;
 
 client.once("ready", () => {
   console.log("Running...");
 });
 
 client.on("message", async (message: Message) => {
-    const [command, ...args] = message.content.trim().toLowerCase().slice(PREFIX.length).split(" ");
-    if(!message.content.startsWith(PREFIX) || message.guild.id !== "775030836868808724") return;
+  const [command, ...args] = message.content
+    .trim()
+    .toLowerCase()
+    .slice(PREFIX.length)
+    .split(" ");
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
+  try {
+    message.channel.startTyping()
+    commands.get(command).execute(message, args);
+    message.channel.stopTyping()
+  } catch {}
 });
 
 client.login();
