f3157af8202c5a8c63b2ad1afc163f766783541f
NajeebFarhan <54494073+CharmfulProgrammer@users.noreply.github.com>
2021-06-07 16:06:54 +0530

    i was so retarted that i couldn't figured out it was so simple

--- src/libs/command.ts
+++ src/libs/command.ts
@@ -1,29 +1,17 @@
 import { Collection } from "discord.js";
 import glob from "glob";
 
-type cmd = { name: string; execute: () => any };
-const commands: Collection<string, cmd> = new Collection();
+const commands: Collection<string, any> = new Collection();
 
-const findCommandFile = () =>
-  new Promise<string[]>((resolve, reject) => {
-    glob("./@(dist|src)/commands/**/*.*", async (err, files) => {
-      if (err) reject(err);
-      resolve(files);
-    });
-  });
-
-const filesRelativePath = async () => {
-  const files = await findCommandFile();
-  const edited = files.map(
-    (f) => "../" + f.split(/\\|\//).reverse().slice(0, 3).reverse().join("/")
+const commandFiles = glob
+  .sync("./@(dist|src)/commands/**/*.*")
+  .map(
+    (f) => "../" + f.split(/[/\\]/g).reverse().slice(0, 3).reverse().join("/")
   );
-  return edited;
-};
 
-const insertCommand = async() => {
-  (await filesRelativePath()).forEach(file => {
-    console.log(file)
-  })
-}
+commandFiles.forEach((file) => {
+  const command = require(file).default;
+  commands.set(command.name, command);
+});
 
-insertCommand()
+export default commands;
