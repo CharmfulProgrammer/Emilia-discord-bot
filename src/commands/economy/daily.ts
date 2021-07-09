398a564cc33cba3ce599a0a954b71689fe5d46c1
NajeebFarhan <54494073+CharmfulProgrammer@users.noreply.github.com>
2021-07-09 14:15:57 +0530

    some economy commands done

--- src/commands/economy/daily.ts
+++ src/commands/economy/daily.ts
@@ -1,14 +1,25 @@
 import command from "types/commandType";
 import User from "../../database/models/bot_user";
 
+const miliSecsInHumanReadableFormat = (miliSecs) => {
+  
+    const inHours = Math.floor(miliSecs/60/60/1000);
+    const inMins = Math.floor(miliSecs/60/1000%60);
+    const inSecs = Math.floor(miliSecs/1000%60)
+    
+    const hrAndMin = (inHours && (inHours + " hours and ")|| "") + ((inMins > 1) && (inMins + " minutes") || "");
+    const secs = inSecs + " seconds"
+    return hrAndMin || secs;
+}
+
 export default {
   name: "daily",
-  description: "Take 200<a:diamonds:862417192012087343> after every 24 hours",
+  description: "Take 200<a:diamonds:862417192012087343> after every 12 hours",
   usage: "daily",
   alias: ["weekly"],
   async execute(message) {
     const timeNow = +new Date();
-    const dayInMili = 24 * 60 * 60 * 1000;
+    const dayInMili = 12 * 60 * 60 * 1000;
     try {
       let user = await User.findOne({
         where: {
@@ -21,14 +32,7 @@ export default {
       const timePassed = timeNow - Number(user.coinCollectedAt);
       console.log(timePassed, dayInMili, timeNow, user.coinCollectedAt, user.coins);
       if (timePassed < dayInMili) {
-        const inMili = dayInMili - timePassed
-        const timeRemaining = {
-          inHours: Math.floor(inMili/60/60/1000),
-          inMins: Math.floor(inMili/60/1000%60),
-          inSecs: Math.floor(inMili/1000%60)
-        }
-        const {inHours, inMins, inSecs} = timeRemaining;
-        return message.channel.send(`You can take your daily after ${inHours}:${inMins}:${inSecs}`);
+        return message.channel.send(`You can collect your daily after ${miliSecsInHumanReadableFormat(dayInMili - timePassed)}`);
       }
 
       await user.increment({
